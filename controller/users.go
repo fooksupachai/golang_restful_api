daf74d3dfc15b8ab2f3cc399c906e5ae6c0c3cf5
fookfik <fookfik@localhost.localdomain>
2020-04-16 17:45:48 +0700

    Create Get All users api

---
+++ controller/users.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Account for describe infomation
+type Account struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Age       int    `json:"age"`
+	Address   string `json:"address"`
+}
+
+// GetUsers to Get all user in database
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+
+	var account []Account
+
+	accountList := Account{"Fook", "Fik", 23, "Detail"}
+
+	for i := 0; i < 10; i++ {
+		account = append(account, accountList)
+	}
+
+	resp := struct {
+		Account []Account `json:"account"`
+	}{
+		Account: account,
+	}
+
+	json.NewEncoder(w).Encode(resp)
+
+}
